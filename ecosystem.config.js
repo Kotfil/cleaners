d25980db1ca92c0bc8e7c3d75ba3a292d5c516be
Kotfil <filippkotenko@outlook.com>
2026-01-07 22:43:33 +0200

    updt ecosys

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       args: 'start',
       env: {
         NODE_ENV: 'production',
-        PORT: 3006,
+        PORT: 3000, // Был 3006
         NODE_OPTIONS: '--max-old-space-size=128'
       },
       instances: 1,
@@ -19,30 +19,14 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
     },
     {
-      name: 'client-portal',
-      cwd: '/cleaners/client-portal',
-      script: 'npm',
-      args: 'start',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3009,
-        NODE_OPTIONS: '--max-old-space-size=128'
-      },
-      instances: 1,
-      exec_mode: 'fork',
-      watch: false,
-      max_memory_restart: '150M',
-      error_file: '/root/.pm2/logs/client-portal-error.log',
-      out_file: '/root/.pm2/logs/client-portal-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-    },
-    {
       name: 'server-crm',
       cwd: '/cleaners/server-crm',
       script: 'npm',
       args: 'run start:prod',
       env_file: '/cleaners/server-crm/.env',
       env: {
+        NODE_ENV: 'production',
+        PORT: 3001, // Явно задаем порт
         NODE_OPTIONS: '--max-old-space-size=180'
       },
       instances: 1,
@@ -54,12 +38,32 @@ module.exports = {
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
     },
     {
+      name: 'client-portal',
+      cwd: '/cleaners/client-portal',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3002, // Был 3009
+        NODE_OPTIONS: '--max-old-space-size=128'
+      },
+      instances: 1,
+      exec_mode: 'fork',
+      watch: false,
+      max_memory_restart: '150M',
+      error_file: '/root/.pm2/logs/client-portal-error.log',
+      out_file: '/root/.pm2/logs/client-portal-out.log',
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+    },
+    {
       name: 'server-client-portal',
       cwd: '/cleaners/server-client-portal',
       script: 'npm',
       args: 'run start:prod',
       env_file: '/cleaners/server-client-portal/.env',
       env: {
+        NODE_ENV: 'production',
+        PORT: 3003, // Явно задаем порт
         NODE_OPTIONS: '--max-old-space-size=180'
       },
       instances: 1,
@@ -68,58 +72,6 @@ module.exports = {
       max_memory_restart: '200M',
       error_file: '/root/.pm2/logs/server-client-portal-error.log',
       out_file: '/root/.pm2/logs/server-client-portal-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-    },
-    {
-      name: 'portfolio-server',
-      cwd: '/Portfolio/server',
-      script: 'npm',
-      args: 'run start:prod',
-      env_file: '/Portfolio/server/.env',
-      env: {
-        NODE_OPTIONS: '--max-old-space-size=180'
-      },
-      instances: 1,
-      exec_mode: 'fork',
-      watch: false,
-      max_memory_restart: '200M',
-      error_file: '/root/.pm2/logs/portfolio-server-error.log',
-      out_file: '/root/.pm2/logs/portfolio-server-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-    },
-    {
-      name: 'main-crm-client',
-      cwd: '/main-crm/client',
-      script: 'npm',
-      args: 'start',
-      env: {
-        NODE_ENV: 'production',
-        PORT: 3004,
-        NODE_OPTIONS: '--max-old-space-size=128'
-      },
-      instances: 1,
-      exec_mode: 'fork',
-      watch: false,
-      max_memory_restart: '150M',
-      error_file: '/root/.pm2/logs/main-crm-client-error.log',
-      out_file: '/root/.pm2/logs/main-crm-client-out.log',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
-    },
-    {
-      name: 'main-crm-server',
-      cwd: '/main-crm/server',
-      script: 'npm',
-      args: 'run start:prod',
-      env_file: '/main-crm/server/.env',
-      env: {
-        NODE_OPTIONS: '--max-old-space-size=180'
-      },
-      instances: 1,
-      exec_mode: 'fork',
-      watch: false,
-      max_memory_restart: '200M',
-      error_file: '/root/.pm2/logs/main-crm-server-error.log',
-      out_file: '/root/.pm2/logs/main-crm-server-out.log',
       log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
     }
   ]
